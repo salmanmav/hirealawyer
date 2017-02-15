38fab86fe15e6dec2df9249aafe7730ffe90c847
furqanharoon <furqanh91@gmail.com>
2017-02-16 03:54:07 +0500

    Fixed issues

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   
   
-  root 'clients#index'
+  
   
   devise_for :lawyers, controllers: { registrations: 'lawyers_devise/registrations', sessions: 'lawyers_devise/sessions' }
   devise_for :clients, controllers: { registrations: 'clients/registrations' }
@@ -13,5 +13,11 @@ Rails.application.routes.draw do
   	end
   end
 
+  devise_scope :lawyers do
+    authenticated  do
+      root to: 'lawyers#index', as: 'authenticated_lawyer_root'
+    end
+  end
+  root 'clients#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
