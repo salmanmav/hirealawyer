bb3d98aea1e7d4ac50197e56bed5b86191c2f3fb
Furqan Haroon <furqan.haroon@kwanso.com>
2017-02-15 12:22:37 +0500

    Add devise route for clients

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  root 'clients#index'
 
+  devise_for :clients, controllers: { registrations: 'clients/registrations' }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
