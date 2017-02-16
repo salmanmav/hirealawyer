2306d8b031afc6806be522543e85b667011c69ea
furqanharoon <furqanh91@gmail.com>
2017-02-16 11:53:00 +0500

    Fixed issues

--- app/controllers/clients_controller.rb
+++ app/controllers/clients_controller.rb
@@ -1,7 +1,7 @@
 class ClientsController < ApplicationController
-  before_action :authenticate_client!
+  before_action :authenticate_client!, except: [:index]
 
   def index
-    @client = current_client
+    # @client = current_client
   end
 end
